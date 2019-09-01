8d757a7c104787107fc69214da7513b617121bbe
moisesBenites <moisesbenites3@gmail.com>
2019-09-01 14:35:48 -0600

    primer commit

---
+++ src/app/pages/dashboar/dashboar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboar',
+  templateUrl: './dashboar.component.html',
+  styles: []
+})
+export class DashboarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
